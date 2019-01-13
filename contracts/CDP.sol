a9af58f71ba148273326a477aedc47f033d15f2f
Carter <cartercarlson94@gmail.com>
2019-01-12 21:31:29 -0600

    Add first modifier, event, and function

--- contracts/CDP.sol
+++ contracts/CDP.sol
@@ -12,11 +12,13 @@ contract CDP {
 
 
     /* EVENTS */
-
+    event logDeposit (uint id);
 
 
 
     /* MODIFIERS */
+    // validate the CDP id exists
+    modifier CDPExists(uint _id) { require( oraclize()     ) }
 
 
 
@@ -42,6 +44,9 @@ contract CDP {
     function addCDP(uint _id)
         public
     {
+        // 1. validate id requested is less than total id's available
+        // 2. validate that for the id requested, the message sender is the same
+        //      as the CDP owner address.
 
     }
 
--- py/models.py
+++ py/models.py
@@ -1,3 +1,25 @@
+INITIAL_INVESTMENT = 5000
+
 
 class CDP(object):
     return
+
+
+
+
+
+'''
+1. fetch current $ETH price
+2. set criteria of investment in either $ETH or $USD
+3. initialize "lock up" of funds
+4. define liquidation price based on:
+    a. $ value of locked up funds,
+    b. $ value withdrew from locked funds
+    c. use liquidation ratio from MakerDAO
+    d. define "bite" penalty
+        > if "bite" happens, convert $ETH into $USD then incur the 13% bite fee.
+        
+
+
+
+'''
