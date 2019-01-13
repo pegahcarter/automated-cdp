dff5dbb978a42c9b370579932c7de3e650bea4f9
Carter <cartercarlson94@gmail.com>
2019-01-12 21:13:51 -0600

    Add skeleton contract

--- contracts/CDP.sol
+++ contracts/CDP.sol
@@ -1,1 +1,53 @@
 pragma solidity ^0.4.20;
+
+contract CDP {
+
+    /* DECLARING VARIABLES */
+    address public owner;
+
+
+    /* MAPPING */
+
+
+
+
+    /* EVENTS */
+
+
+
+
+    /* MODIFIERS */
+
+
+
+
+    /* STRUCTS */
+
+
+
+
+    /* CONSTRUCTUR */
+    cunstructor() public {
+        owner = msg.sender;
+    }
+
+
+
+    /* VIEWS */
+
+
+
+
+    /* METHODS */
+    function addCDP(uint _id)
+        public
+    {
+
+    }
+
+
+
+
+
+
+}
