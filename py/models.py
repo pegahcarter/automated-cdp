60993ff6781db4e8dfd05077b1a949d1692b660c
Carter <cartercarlson94@gmail.com>
2019-01-14 07:54:02 -0600

    Add brainstorming for CDP workflow

--- py/models.py
+++ py/models.py
@@ -1,3 +1,12 @@
+from web3 import HTTPProvider, Web3
+
+from pymaker import Address
+from pymaker.token import ERC20Token
+from pymaker.numeric import Ray
+from pymaker.sai import Tub, Tap
+
+
+
 INITIAL_INVESTMENT = 5000
 
 
@@ -18,7 +27,7 @@ class CDP(object):
     c. use liquidation ratio from MakerDAO
     d. define "bite" penalty
         > if "bite" happens, convert $ETH into $USD then incur the 13% bite fee.
-        
+
 
 
 
