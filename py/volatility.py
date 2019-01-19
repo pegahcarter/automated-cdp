a924602f4aaeeecb751a35e170f4174ec6e2d280
Carter <cartercarlson94@gmail.com>
2019-01-18 19:06:36 -0600

    Remove react example and start calculating volatility

--- py/volatility.py
+++ py/volatility.py
@@ -1,3 +1,15 @@
 '''
 This file is used to calculate daily volatility, EMA's, etc.
 '''
+import os
+import sys
+from datetime import datetime
+import pandas as pd
+import numpy as np
+
+
+FILE = '../data/prices.csv'
+df = pd.read_csv(FILE)
+df.head()
+
+data['Differences'] = data['High'] - data['Low']
