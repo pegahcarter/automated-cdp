51957848996a9c16366b384751e6f91d65c57197
Carter <cartercarlson94@gmail.com>
2019-01-20 16:12:59 -0600

    add __init__.py

--- py/cdp.py
+++ py/cdp.py
@@ -1,31 +1,32 @@
-
-import os
+import math
 import numpy as np
 import pandas as pd
-
-START_AMT = 5000
-
-prices = pd.read_csv('../data/prices.csv')
-
-
-
-
-
-
-
-
-
 
 
 class CDP(object):
 
     def __init__(self):
-        self.deposited = START_AMT
+        self.deposited = 0
         self.liquidation_price = 0
         self.generated = 0
+        self.fees = 0
 
+    def deposit(self, eth):
+        self.deposited += eth
+
+
+    def withdraw(self, eth):
+        if eth > self.deposited:
+            return "Cannot withdraw more than the max."
+
+        self.deposited -= eth
+
+    def calc_liquidation_price(self):
         pass
 
+    def generate(self, dai):
 
+        if self.liquidation_price > current_price:
+            return "DAI generation would liquidate account.  Stopping early."
 
-prices.head()
+        pass
