85d1858899a7010eabdc4649a65e83183e37963b
Carter Carlson <carlfarterson@gmail.com>
2019-05-20 12:46:48 -0400

    Create first graph of performance

---
+++ py/graph.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/cdp_comparison.csv')
+
+fig, ax = plt.subplots(figsize=(5,5))
+
+plt.plot(df['Non-CDP'], color='black')
+plt.plot(df['CDP-1'], color='red')
+plt.plot(df['CDP-2'], color='blue')
+plt.legend()
+plt.show()
+
+results = pd.DataFrame([df.iloc[i] for i in range(0, len(df), 150)])
+results.index += 100
+print(results)
--- py/main.py
+++ py/main.py
@@ -17,10 +17,9 @@ def main():
 
 
     for i in range(100, 1000):
-        non_CDP = i
         CDP_1 = cdp_1.close(price=i) * i
         CDP_2 = cdp_2.close(price=i) * i
-        df.append([non_CDP, CDP_1, CDP_2])
+        df.append([i, CDP_1, CDP_2])
 
     df = pd.DataFrame(df, columns=['Non-CDP', 'CDP-1', 'CDP-2'])
     df.to_csv('data/cdp_comparison.csv', index=False)
