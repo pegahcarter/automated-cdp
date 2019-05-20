b29c5c4752c660885f09e42f79fb52f16cd171f4
Carter Carlson <carlfarterson@gmail.com>
2019-05-20 17:31:59 -0400

    Add 3rd simulation

--- py/graph.py
+++ py/graph.py
@@ -1,16 +1,33 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Withdrawal 1: $33.333...
+# Withdrawal 2: $27.777...
+
+# log_list = []
+# val = 0
+# for i in range(900):
+#     val += (1.0/2.0)**i
+#     log_list.append(val)
+
 df = pd.read_csv('data/simulations/basic_comparison.csv')
-
+df.set_index('Price', inplace=True)
 fig, ax = plt.subplots(figsize=(5,5))
-
-plt.plot(df['Non-CDP'], color='black')
 plt.plot(df['CDP-1'], color='red')
 plt.plot(df['CDP-2'], color='blue')
+plt.plot(df['CDP-3'], color='green')
 plt.legend()
-plt.show()
 
-results = pd.DataFrame([df.iloc[i] for i in range(0, len(df), 150)])
-results.index += 100
-print(results)
+# Basic summary of every 150 data points
+pd.DataFrame([df.iloc[i] for i in df.index if (i+1) % 150 == 0])
+
+df_pct = pd.DataFrame()
+df_pct['CDP-1'] = (df['CDP-1'] - df.index) / df.index
+df_pct['CDP-2'] = (df['CDP-2'] - df.index) / df.index
+
+plt.plot(df_pct['CDP-1'], color='red')
+plt.plot(df_pct['CDP-2'], color='blue')
+plt.legend()
+
+df_pct['CDP-1'].iat[899]
+df_pct['CDP-2'].iat[899]
--- py/main.py
+++ py/main.py
@@ -11,19 +11,24 @@ def main():
     cdp_2 = CDP()
     cdp_2.generate_usd(withdrawal_1)
     cdp_2.deposit_eth(eth=withdrawal_1/cdp_2.price)
-    withdrawal_2 = cdp_2.usd_available_to_generate * 0.5
-    cdp_2.generate_usd(withdrawal_2)
-    cdp_2.deposit_eth(eth=withdrawal_2/cdp_2.price)
+    cdp_2.generate_usd(withdrawal_1)
+    cdp_2.deposit_eth(eth=withdrawal_1/cdp_2.price)
 
+    cdp_3 = CDP()
+    cdp_3.generate_usd(withdrawal_1)
+    cdp_3.deposit_eth(eth=withdrawal_1/cdp_3.price)
+    withdrawal_2 = cdp_3.usd_available_to_generate * .5
+    cdp_3.generate_usd(withdrawal_2)
+    cdp_3.deposit_eth(eth=withdrawal_2/cdp_3.price)
 
     for i in range(100, 1000):
         CDP_1 = cdp_1.close(price=i) * i
         CDP_2 = cdp_2.close(price=i) * i
-        df.append([i, CDP_1, CDP_2])
+        CDP_3 = cdp_3.close(price=i) * i
+        df.append([i, CDP_1, CDP_2, CDP_3])
 
-    df = pd.DataFrame(df, columns=['Non-CDP', 'CDP-1', 'CDP-2'])
+    df = pd.DataFrame(df, columns=['Price', 'CDP-1', 'CDP-2', 'CDP-3'])
     df.to_csv('data/simulations/basic_comparison.csv', index=False)
-
 
 if __name__ == '__main__':
     main()
