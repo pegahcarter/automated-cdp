19d07fc34b28db86ad138557ab02d8a83c8e8488
Carter Carlson <carlfarterson@gmail.com>
2019-05-21 14:20:52 -0400

    Uncomment CDP functions and create testing file

--- py/cdp.py
+++ py/cdp.py
@@ -1,30 +1,44 @@
+'''
+Brainstorming to document CDP actions to CSV
+
+Columns
+    - Date
+    - Action
+    - ETH_USD
+    - Quantity
+
+How do we account for tx fees?
+
+Is there a way to apply _update_calculations() to formulas without constantly
+calling it?
+'''
+
+
 class CDP:
 
-    MIN_RATIO = 3.0/2.0
-
-    def __init__(self, price=100):
-        self.usd_on_hand = 0
-        self.usd_generated = 0
-        self.eth_deposited = 1
+    def __init__(self, price):
+        self.start_price = price
         self.price = price
-        # self.eth_on_hand = 0
-        # self.start_eth_on_hand = eth_on_hand - self.eth_deposited
-        # self.start_price = price
-        self._update_calculations()
+        self.eth_on_hand = 0
+        self.eth_deposited = 0
+        self.usd_generated = 0
+        self.usd_value = 0
+        self.usd_available_to_generate = 0
+        self.liquidation_price = 0
 
     def _update_calculations(self):
-        # self.liquidation_price = self.usd_generated * self.MIN_RATIO / self.eth_deposited
+        self.liquidation_price = self.usd_generated * self.MIN_RATIO / self.eth_deposited
         self.usd_value = self.price * self.eth_deposited
         self.usd_available_to_generate = self.usd_value / self.MIN_RATIO - self.usd_generated
 
     def deposit_eth(self, eth):
         self.eth_deposited += eth
-        # self.eth_on_hand -= eth
+        self.eth_on_hand -= eth
         self._update_calculations()
 
-    def transfer_usd(self, usd):
-        self.usd_transferred_to_carter += usd
-        self.usd_on_hand -= usd
+    def withdraw_eth(self, eth):
+        self.eth_deposited -= eth
+        self.eth_on_hand += eth
         self._update_calculations()
 
     def generate_usd(self, usd):
@@ -37,7 +51,7 @@ class CDP:
         self.usd_on_hand -= usd
         self._update_calculations()
 
-    def update_eth_price(self, price):
+    def update_price(self, price):
         self.price = price
         self._update_calculations()
 
