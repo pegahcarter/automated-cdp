07fbb0e31b95d9dad52ad78a4c9ba38063e96f81
Carter Carlson <carlfarterson@gmail.com>
2019-05-29 12:33:52 -0500

    Create basic homepage components

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Homepage from './home-page'
+
+class App extends Component {
+
+  state = {
+    loggedIn: true
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let { loggedIn } = this.state;
+    return (
+      <div>'Hello'</div>
+    )
+  }
+
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ interface/src/home-page/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Summary from './Summary'
+import Actions from './Actions'
+import Trades from './Trades'
+
+const Homepage = ({ loggedIn }) => {
+  return (
+    <div>
+      <Summary />
+      <Actions />
+      <Trades />
+    </div>
+  )
+}
