0e8dc14b3722aa2ec2caa6acc068fd56b2e15ccd
Carter Carlson <carlfarterson@gmail.com>
2019-05-29 14:38:04 -0500

    Remove template files/comments

--- interface/src/App.js
+++ interface/src/App.js
@@ -15,31 +15,10 @@ class App extends Component {
   render() {
     let { loggedIn } = this.state;
     return (
-      <div>'Hello'</div>
+      <div>Hello</div>
     )
   }
 
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
