e3481ca7c721a6c3e13c6df615e485fca10fcffb
Carter Carlson <carlfarterson@gmail.com>
2019-06-03 00:58:48 -0500

    Successfully displayed actions using react-table

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import './App.css';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 import Homepage from './home-page';
 import cdp from './home-page/cdp.json';
 
@@ -16,8 +18,43 @@ class App extends Component {
   }
 
   render() {
+    const columns = [
+      {
+        Header: 'Trade ID',
+        accessor: 'id',
+        width: 100
+      },
+      {
+        Header: 'Date',
+        accessor: 'date'
+      },
+      {
+        Header: 'Action',
+        accessor: 'action',
+        sortable: false,
+        width: 125
+      },
+      {
+        Header: 'ETH-USD',
+        accessor: 'eth-usd',
+        sortable: false,
+        width: 100
+      },
+      {
+        Header: 'Quantity',
+        accessor: 'quantity',
+        sortable: false,
+      }
+    ]
     return (
-      <Homepage />
+      // <Homepage />
+      <ReactTable
+        columns={columns}
+        data={this.state.actions}
+        defaultPageSize={5}
+      >
+
+      </ReactTable>
     )
   }
 
