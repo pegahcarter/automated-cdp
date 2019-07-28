5885998efc1472cb7a73df9b6142f9e21360bc1c
Carter Carlson <carlfarterson@gmail.com>
2019-07-28 11:47:35 -0500

    Remove nested npm directory

--- py/main.py
+++ py/main.py
@@ -1,17 +1,12 @@
 import pandas as pd
 from py.cdp import CDP
 
-cdp = CDP(price=225, eth_deposited=85.722)
-withdrawal_1 = cdp.summary['usd_available_to_generate'] * 0.
+cdp = CDP(price=208.05, eth_deposited=85.722)
+withdrawal_1 = 5488
 cdp.generate(withdrawal_1)
-eth_purchased = cdp.trade(side='BUY', usd=withdrawal_1, price=228)
-cdp.deposit(eth=cdp.summary['eth_on_hand'])
-cdp.summarize(price=350, save=True)
-
-
-
-
-
+cdp.trade(side='BUY', usd=withdrawal_1, price=228)
+cdp.deposit(eth=23.03)
+cdp.summarize(price=300, save=True)
 
 
 if __name__ == '__main__':
